de324532a8ce4a3c42d44eba8f98563788c13d16
Christiaan de Die le Clercq <christiaan@uncinc.nl>
2021-12-16 11:00:01 +0100

    Set  env to true to only log change

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This was made to have Kubernetes always use a static external IP on DigitalOcean
 Instructions can be found here: https://techwolf12.nl/blog/digitalocean-kubernetes-static-ipv4
 
  
+Set SILENT env var to true to only log changes 
+
--- kube-set-route.sh
+++ kube-set-route.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
-echo "Route from INETROUTE env: $INETROUTE"
 CURRENTROUTE=`nsenter -t 1 -m -u -n -i -- sh -c 'ip route show default' | awk -F ' ' '/default/ {print $3}'`
-echo "Current route: $CURRENTROUTE"
-
+if [ "$SILENT" != "true"]
+then
+  echo "Route from INETROUTE env: $INETROUTE"
+  echo "Current route: $CURRENTROUTE"
+fi
 
 if [ "$CURRENTROUTE" = "$INETROUTE" ]
 then
-  echo "Current and INETROUTE are the same, doing nothing"
+  if [ "$SILENT" != "true"]
+  then
+    echo "Current and INETROUTE are the same, doing nothing"
+  fi
 else
   echo "Changing route from $CURRENTROUTE to $INETROUTE"
   nsenter -t 1 -m -u -n -i -- sh -c "ip route change default via $INETROUTE"
 fi
 
-echo -e "\n---\n"
+if [ "$SILENT" != "true"]
+then
+  echo -e "\n---\n"
+fi
